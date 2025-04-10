d511fe28a7400556e3e1b599aecba5816f3c7e2b
Ahmed-Zakaria77 <aziko882@gmail.com>
2025-04-10 05:01:22 +0200

    first commit

--- j.s/main.js
+++ j.s/main.js
@@ -14,3 +14,71 @@ document.addEventListener("DOMContentLoaded", function () {
       ride: 'carousel'
   });
 });
+
+
+const scrollBtn = document.getElementById("scrollToTopBtn");
+const heroSection = document.getElementById("home");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // المستخدم في أول سيكشن → خفي الزر
+        scrollBtn.classList.remove("show");
+      } else {
+        // خرج من أول سيكشن → أظهر الزر
+        scrollBtn.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.1
+  }
+);
+
+observer.observe(home);
+
+scrollBtn.addEventListener("click", () => {
+  document.documentElement.scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+const popup = document.getElementById('imgPopup');
+  const popupImg = document.getElementById('popupImage');
+  const closeBtn = document.querySelector('.popup .close');
+
+  document.querySelectorAll('.popup-img').forEach(img => {
+    img.addEventListener('click', () => {
+      popupImg.src = img.src;
+      popup.style.display = 'flex';
+    });
+  });
+
+  closeBtn.addEventListener('click', () => {
+    popup.style.display = 'none';
+  });
+
+  popup.addEventListener('click', (e) => {
+    if (e.target === popup) {
+      popup.style.display = 'none';
+    }
+  });
+
+  function playSound() {
+    const audio = document.getElementById("myAudio");
+    audio.play();
+  }
+
+  window.addEventListener("load", () => {
+    setTimeout(() => {
+      const preloader = document.getElementById("preloader");
+      preloader.classList.add("fade-out");
+
+      // بعد نهاية التأثير، نخفي العنصر تمامًا ونظهر المحتوى
+      setTimeout(() => {
+        preloader.style.display = "none";
+        document.getElementById("mainContent").style.display = "block";
+      }, 500); // مدة الأنيميشن (مطابقة للـ transition في CSS)
+    }, 3000); // مدة شاشة التحميل (ثانيتين)
+  });
