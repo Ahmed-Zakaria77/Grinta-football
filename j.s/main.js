33b1e35d7fd67eb5fa7c35f1394c5f6236d5e911
Ahmed-Zakaria77 <aziko882@gmail.com>
2025-05-23 18:07:52 +0300

    New commit

--- j.s/main.js
+++ j.s/main.js
@@ -82,3 +82,12 @@ const popup = document.getElementById('imgPopup');
       }, 500);
     }, 3000); 
   });
+
+
+   document.getElementById('pageSelect').addEventListener('change', function() {
+      var selectedPage = this.value;
+      if (selectedPage) {
+        window.location.href = selectedPage;
+      }
+    });
+
